4f609292cec44a621d074ea8bfe252fd0afa0864
beksar <beksar1998@mail.ru>
2024-06-26 10:00:21 +0500

    advert photo

---
+++ src/main/kotlin/com/beksar/market/services/ads/controllers/ProductPhotoController.kt
@@ -0,0 +1,31 @@
+package com.beksar.market.services.ads.controllers
+
+import com.beksar.market.core.extentions.response
+import com.beksar.market.core.models.base.BaseResponse
+import com.beksar.market.services.ads.models.dto.AddPhotoRequest
+import com.beksar.market.services.ads.service.AdvertPhotoService
+import org.springframework.web.bind.annotation.*
+
+
+@RestController
+@RequestMapping("api/v1/advert/photo")
+class ProductPhotoController(private val service: AdvertPhotoService) {
+
+
+    @PostMapping("{productId}")
+    fun uploadPhoto(
+        @PathVariable productId: String,
+        @RequestBody addPhotoRequest: AddPhotoRequest,
+    ): BaseResponse<Boolean> {
+        service.uploadPhoto(productId, addPhotoRequest)
+        return true.response()
+    }
+
+    @DeleteMapping("{name}")
+    fun deletePhoto(@PathVariable name: String): BaseResponse<Boolean> {
+        service.delete(name)
+        return true.response()
+    }
+
+
+}
