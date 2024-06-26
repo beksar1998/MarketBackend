a03d3c8cf585f78d91b4f6cae00771456c7fbe97
beksar <beksar1998@mail.ru>
2024-06-26 14:44:09 +0500

    RENAME

--- src/main/kotlin/com/beksar/market/services/ads/models/dto/AddOrUpdateAdvertRequest.kt
+++ src/main/kotlin/com/beksar/market/services/ads/models/dto/AddOrUpdateAdvertRequest.kt
@@ -1,6 +1,6 @@
 package com.beksar.market.services.ads.models.dto
 
 class AddOrUpdateAdvertRequest(
-    val title: String? = "",
+//    val title: String? = "",
     val description: String? = "",
 )
